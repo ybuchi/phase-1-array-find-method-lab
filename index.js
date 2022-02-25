c430cbff95a4359df1ff384110840acfb1eb808f
ybuchi <yabuch_ch@hotmail.com>
2022-02-25 10:38:22 -0500

    commit tree 015c4f9fcf54bb976f3460857c3d4b3725e50397

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"}
+]
+
+// function superbowlWin(arr){
+
+// }
+function superbowlWin(arr){
+    //Define the function that will be passed into the find method.
+    function isWin(element, index, array){
+        return(element.result === "W")
+    }
+    //Store the find array and find method into a variable
+    //This returns the full object element;
+    const element = arr.find(isWin);
+    
+    if (typeof element === "object"){
+        return element.year;
+    }else{
+        return undefined;
+    }
+    // return element.year;
+    }
+
+console.log(superbowlWin(record));
+
+
+
+
+
+
+
+
